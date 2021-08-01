f3837654745876734cbee2dc4c92cff713de1fb6
pilotKelvin <pilotkelvin0@gmail.com>
2021-08-01 13:47:43 +0300

    fn_simulate_moving_vehicle() update
    1. Utilized Numpy arrays for the first time to generate points  and axle positions along the simply supported beam as the vehicle moves from one end to the other.
    2. Deleted the original loop based code.

--- moving_load_simulator.py
+++ moving_load_simulator.py
@@ -10,15 +10,18 @@ from logging import basicConfig, DEBUG, debug,disable
 from sys import argv
 import pprint
 
+import numpy as np
+import pandas as pd
+import sympy as sp
+
 basicConfig(level= DEBUG, format=' %(message)s')
-disable()
+# disable()
 
 getcontext().prec = 2
 
 class MovingLoadSimulator():
     def __init__(self, beam_length, moving_interval, axle_positions):
         """
-        Instantiates instant variables and formats self.results() which is the class output
         :param beam_length:
         :param moving_interval:
         :param axle_positions:
@@ -27,56 +30,33 @@ class MovingLoadSimulator():
         self.moving_interval = moving_interval
         self.axle_positions = axle_positions
 
-        # dictionary to store the output of this class
-        self.simulation_data = {}
-        # Format of the result dictionary will be as follows:
-        '''
-        {
-        step:{
-                'axle_distances': [ ... ]
-                'ordinates': [ ... ]
-             }
-             
-             }
-             ...
-             }
-             ...
-        }
-        '''
-        # construct results dictionary
-        axles_location_dict = self.fn_simulate_moving_vehicle() # generate distances
-        axles_influence_ordinates_dict = self.fn_point_load_ordinates() # generate influence lines
+        self.fn_simulate_moving_vehicle(self.beam_length, self.moving_interval, self.axle_positions)
 
-        for axle_position in axles_location_dict.keys():
-            min_dict = {}
-            min_dict["axle_distances"] = axles_location_dict[axle_position]
-            min_dict["ordinates"] = axles_influence_ordinates_dict[axle_position]
-            # print(axle_position)
-            self.simulation_data[axle_position] = min_dict
+    def fn_simulate_moving_vehicle(self, beam_length, moving_interval, axle_positions):
+        """
+        This function generates position of each axle along the beam as the vehicle move
+        :param beam_length:
+        :param moving_interval:
+        :param axle_positions:
+        :return: axle_location_arr
+        """
+        steps_to_move_arr = np.append(np.arange(0, self.beam_length, self.moving_interval), self.beam_length) # generates points along the beam with moving interval provided
+        no_of_steps_to_move = steps_to_move_arr.size # get the number of points generated
 
-    def fn_simulate_moving_vehicle(self):
-        '''
-        Simulates movement of a vehicle over a simply supported beam.
-        (At the moment the program allows the car to move forward. Effect on reversing especially for SV 196 and SOV should be explored.)
-        Moves the vehicle at the intervals specified/provided and then generate positions for axle in relation to first support.
-        inputs: self.beam_length, self.vehicle_axle_positions, self.moving_interval
-        :return: axles_locations_dict # stores axle positions at each step the vehicle moves
-        '''
-        self.steps_to_move = round((self.beam_length / self.moving_interval) + 1) # determine how many steps of the interval specified the vehicle moves
-        debug(f"No.of {self.moving_interval} metres steps moved = {self.steps_to_move }")
-        cumulative_vehicle_movement = 0 # Accumulating variable for vehicle steps/intervals. Accumulates each step after every loop
+        axle_location_arr = np.empty([no_of_steps_to_move, len(axle_positions)], dtype=float) # initialize an empty to populate with axle location later
 
-        axles_location_dict = {} # dict to store position of all axle at each step
-        for step in range(self.steps_to_move): # loop through the determined step
-            x_distances = [] # clear list beginning of each step to store new axle locations
-            for position in self.axle_positions: # for every step find location of each axle in relation to first support
-                axle_position = round((cumulative_vehicle_movement - position),2)
-                x_distances.append(axle_position) # append each axle location in a list
+        for index, point in enumerate(steps_to_move_arr): # find axle positions of each point generated
+            axle_location_arr[index][0] = point
+            for index_, axle in enumerate(axle_positions):
+                if axle == 0:
+                    pass
+                else:
+                    position = round(point - axle, 2)
+                    axle_location_arr[index][index_] = position
 
-            axles_location_dict[step+1] = x_distances # add list of axle position for each step to axle location dict
-            cumulative_vehicle_movement += self.moving_interval # move vehicle to next position
+        debug(f"{axle_location_arr}")
 
-        return axles_location_dict # return dic with all axle positions at each step
+        return axle_location_arr # return numpy array with all axle positions at each point along the beam
 
     def fn_point_load_ordinates(self):
         '''
@@ -110,26 +90,13 @@ if __name__ == '__main__':
 
     beam_length = 30
     moving_interval = 0.5
-    axle_positions = [0, 1.2,2.4,3.6,4.8,6]
+    axle_positions = [0, 1.2, 2.4, 3.6, 4.8, 6.0]
 
     simulator = MovingLoadSimulator(beam_length, moving_interval, axle_positions)
 
-    pprint.pprint(simulator.simulation_data)
 
-# Next assignment - Done 05/04/2021
-# 1. Test generation of special vehicle influence ordinate
-     # Testing outcome
-        #  improve fn_simulate_moving_vehicle() to generate position of all axle in rlt to first support
+# Next assignment - Tue 03/08/2021
+# 1. Refactor self.fn_simulate_moving_vehicle() especially output data type. Consider Series or dataFrame as output
 
-# Next task - Done 06/04/2021
-# 1. Improve fn_simulate_moving_vehicle() to generate position of all axle in relation to first support and repeat testing for LM1, LM2 and LM3
-# 2. Update fn_point_load_ordinates() for changes in task 1
-# 3. Improve the storage of influence ordinates
-# 4. Research on best data structure for storing lots of data
-
-# Next task - Done 08/05/2021
-# 1. Test
-# 2. Refactor
-# 3. More documentation
 
 
