d4c25183388ef784a9c41cd47e8cfaf20b9c52f9
pilotKelvin <pilotkelvin0@gmail.com>
2021-08-02 13:06:54 +0300

    -Added comments -Merged the 2 original functions for calculating axle locations and influence ordinates into one function -Implemented code for calculating influence ordinates using array boolean indexing

--- moving_load_simulator.py
+++ moving_load_simulator.py
@@ -5,7 +5,7 @@ Author: Kelvin Macharia W
 Year: August 2021
 """
 from decimal import Decimal, getcontext
-from logging import basicConfig, DEBUG, debug,disable
+from logging import basicConfig, DEBUG, debug, disable
 from sys import argv
 import pprint
 
@@ -13,12 +13,13 @@ import numpy as np
 import pandas as pd
 import sympy as sp
 
-basicConfig(level= DEBUG, format=' %(message)s')
+basicConfig(level=DEBUG, format=' %(message)s')
 # disable()
 
 getcontext().prec = 2
 
-class MovingLoadSimulator():
+
+class MovingLoadSimulator:
     def __init__(self, beam_length, moving_interval, axle_positions):
         """
         :param beam_length:
@@ -31,68 +32,58 @@ class MovingLoadSimulator():
 
         self.fn_simulate_moving_vehicle(self.beam_length, self.moving_interval, self.axle_positions)
 
-    def fn_simulate_moving_vehicle(self, beam_length, moving_interval, axle_positions):
+    def fn_simulate_moving_vehicle(self, beam_length_: float, moving_interval_: float, axle_positions_: list) -> np:
         """
         This function generates position of each axle along the beam as the vehicle move
-        :param beam_length:
-        :param moving_interval:
-        :param axle_positions:
+        :param beam_length_:
+        :param moving_interval_:
+        :param axle_positions_:
         :return: axle_location_arr
         """
-        steps_to_move_arr = np.append(np.arange(0, self.beam_length, self.moving_interval), self.beam_length) # generates points along the beam with moving interval provided
-        no_of_steps_to_move = steps_to_move_arr.size # get the number of points generated
+        # generates points along the beam at the moving interval provided
+        steps_to_move_arr = np.append(np.arange(0, beam_length_, moving_interval_), self.beam_length)
+        # get the number of points generated
+        no_of_steps_to_move = steps_to_move_arr.size
+        # initialize an empty array to store axle locations later
+        # Each row stores axle positions for point_x. No. of rows are equal to points generated
+        axle_location_arr = np.empty([no_of_steps_to_move, len(axle_positions_)], dtype=float)
 
-        axle_location_arr = np.empty([no_of_steps_to_move, len(axle_positions)], dtype=float) # initialize an empty to populate with axle location later
-
-        for index, point in enumerate(steps_to_move_arr): # find axle positions of each point generated
-            axle_location_arr[index][0] = point
-            for index_, axle in enumerate(axle_positions):
-                if axle == 0:
+        # find axle positions of each point generated
+        for index, point_x in enumerate(steps_to_move_arr):
+            axle_location_arr[index][0] = point_x  # any point_x along the beam is always the location of the first axle
+            for index_, axle_position in enumerate(axle_positions_):
+                if axle_position == 0:  # first axle position coincides with point_x
                     pass
                 else:
-                    position = round(point - axle, 2)
-                    axle_location_arr[index][index_] = position
+                    position = round(point_x - axle_position,
+                                     2)  # compute each axle position in relation to point_x except for first axle
+                    axle_location_arr[index][index_] = position  # append each axle position to axle_location_arr.
+                    # Each row of the axle_location_arr represents axle positions for point_x
 
-        debug(f"{axle_location_arr}")
+        # compute influence lines
+        influence_ordinates = axle_location_arr.copy()  # make a copy of axle locations
+        influence_ordinates = (beam_length_ - influence_ordinates) / beam_length_  # compute influence line ordinates
+        influence_ordinates[influence_ordinates > 1] = 0  # filter out values greater than one
+        # round off values in the influence ordinate array. Use for loop to round values in each row since round takes 1 D array
+        for index, row in enumerate(influence_ordinates):
+            influence_ordinates[index] = np.round(row, 2)
 
-        return axle_location_arr # return numpy array with all axle positions at each point along the beam
+        debug(f"{axle_location_arr.ndim}")
+        debug("********************")
+        debug(f"{influence_ordinates.ndim}")
 
-    def fn_point_load_ordinates(self):
-        '''
-        calculates ordinates of each axle position at each step the vehicle moves
-        inputs: self.beam_length, axles_location_dict
-        :return: influence_ordinates_dict # stores influence ordinates of every axle at each step the vehicle moves
-        '''
-        axles_location_dict = self.fn_simulate_moving_vehicle()
+        return axle_location_arr  # return numpy array with all axle positions at each point along the beam
 
-        axles_influence_ordinates_dict = {}
-
-        for step in axles_location_dict.keys():
-            ordinates_lst = []
-            # debug(x_distances_dict[step])
-            for axle_position in axles_location_dict[step]: # loop
-                if axle_position < 0:
-                    ordinates_lst.append(0)
-                    # influence_ordinates[step+1] = ordinates
-                else:
-                    distance_from_far_support = self.beam_length - axle_position
-                    ordinate = round(((1 / self.beam_length) * distance_from_far_support),2)  # compute ordinates and append to list
-                    if ordinate < 0: # No ordinates should be negative. Due to round off steps moved. The first axle may move past the other other end giving a negative ordinates.
-                        ordinate = 0.0
-                    ordinates_lst.append(ordinate)
-                axles_influence_ordinates_dict[step] = ordinates_lst
-
-        return axles_influence_ordinates_dict  # return dict with influence ordinates
 
 if __name__ == '__main__':
     # supply testing inputs
     beam_length = 30
     moving_interval = 0.5
-    axle_positions = [0, 1.2, 2.4, 3.6, 4.8, 6.0]
+    axle_positions = [0, 1.2]
 
     simulator = MovingLoadSimulator(beam_length, moving_interval, axle_positions)
 
 # Next assignment - Tue 03/08/2021
 # 1. Refactor self.fn_simulate_moving_vehicle() especially output data type. Consider Series or dataFrame as output
-# 2. See if the two functions in this class can be merged
-# 3. Add comments
+# 2. Add comments
+# 3. Test with more data. Prepare example data output to be checked by Spencer
